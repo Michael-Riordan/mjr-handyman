aabee35b7d35167e574f4864640f73e114cb8800
Michael <michael.riordan81@outlook.com>
2025-07-28 19:50:01 -0700

    edit profile names

--- src/app/who-we-are/page.tsx
+++ src/app/who-we-are/page.tsx
@@ -8,12 +8,12 @@ export default function WhoWeAre() {
             </h1>
             <section className="flex p-6 flex-col md:flex-row md:gap-10 sm:justify-self-center">
                 <ProfileCard 
-                    name="Michael Riordan Jr."
+                    name="Michael Jr."
                     image="/ProfileCardImages/lucy&i.jpg"
                     description={`Using his knowledge and experience in different facets, Michael Jr. has built everything from custom cabinets to an off-grid solar-powered camper van. He's worked as a cabinetmaker, and previously served as the on-call handyman for two residential buildings in New York City. He brings a problem-solving mindset, quality craftsmanship, and a deep respect for your home.`}
                 />
                 <ProfileCard
-                    name="Michael Riordan Sr."
+                    name="Michael Sr."
                     image="/ProfileCardImages/dad-profile-photo.jpg"
                     description={`With over 30 years of experience as an operating engineer in NYC, Michael Sr. has overseen the mechanical and cosmetic upkeep of major commercial buildings as the chief operating engineer for high end tenants, such as Red Bull and X (formerly Twitter). From HVAC systems and electrical infrastructure to drywall repairs and finishing work, he’s done it all. His deep expertise ensures every job is handled with precision, safety, and lasting quality.`}
                 />
