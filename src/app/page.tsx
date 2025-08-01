987e8f92b65070ced187c5c1ac62f0e7e9daf3fd
Michael <michael.riordan81@outlook.com>
2025-07-31 20:01:42 -0700

    try again

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 
 'use client'
+import { MapPin } from "lucide-react";
+
 import Hero from "./components/Hero";
 import ProjectsSection from "./components/ProjectSection";
 
@@ -11,8 +13,9 @@ export default function Home() {
         <h2 className="text-center text-2xl text-blue-900 font-bold p-6">Project Before and Afters</h2>
         <ProjectsSection />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        
+      <footer className="row-start-3 flex gap-2 flex-wrap justify-center bg-gray-50 p-5">
+        <MapPin className="w-5 h-5 text-blue-900 self-center" />
+        <p className="text-blue-900 text-xl font-bold">Phoenix, AZ</p>
       </footer>
     </div>
   );
